7c367c1c68876eeb65495a171dcbe6cc21b262c1
lin <admin@apt.ink>
2023-05-14 22:08:48 +0800

    过备案

---
+++ public/js/main.js
@@ -0,0 +1,26 @@
+window.onload = function(){
+	function $my(id){
+		return document.getElementById(id);
+	}
+	Element.prototype.typewriter=function(a){
+			var d = this,
+				c = d.innerHTML,
+				b = 0;
+			d.innerHTML="";
+			var e = setInterval(function() {
+				var f = c.substr(b, 1);
+				if (f == "<") {
+					b = c.indexOf(">", b) + 1
+				} else {
+					b++
+				}
+				d.innerHTML=c.substring(0, b) + (b & 1 ? "_" : "");
+				if (b >= c.length) {
+					clearInterval(e)
+				}
+			}, 150)
+		return this
+	
+	}
+document.getElementById("code").typewriter();
+}
